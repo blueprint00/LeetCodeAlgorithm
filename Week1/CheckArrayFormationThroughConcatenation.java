a5a62b279633458e935faca50e70dc982d4b7f58
blueprint00 <ayun96@naver.com>
2021-02-01 20:33:23 +0900

    Week1 -> day1

--- Week1/CheckArrayFormationThroughConcatenation.java
+++ Week1/CheckArrayFormationThroughConcatenation.java
@@ -1,4 +1,5 @@
 package Week1;
+import java.util.*;
 
 public class CheckArrayFormationThroughConcatenation {
 	public static void main(String[] args) {
@@ -8,26 +9,43 @@ public class CheckArrayFormationThroughConcatenation {
 	}
 	
 	public static boolean canFormArray(int[] arr, int[][] pieces) {
-		int cnt = 0;
-		for(int i = 0; i < arr.length; i ++) {
-			for(int j = 0; j < pieces.length; j ++) {
-				int length = pieces[j].length;
-				if(arr[i] == pieces[j][0]) {
-					cnt ++;
-					if(length > 1) {
-						cnt --;
-						for(int piece : pieces[j]) {
-							if(arr[i ++] == piece) {
-								if(i == arr.length) break;
-								cnt ++;
-							}
-							i --;
-						}
-					}
-				}
-			}
+//		int cnt = 0;
+//		for(int i = 0; i < arr.length; i ++) {
+//			for(int j = 0; j < pieces.length; j ++) {
+//				int length = pieces[j].length;
+//				if(arr[i] == pieces[j][0]) {
+//					cnt ++;
+//					if(length > 1) {
+//						cnt --;
+//						for(int piece : pieces[j]) {
+//							if(arr[i ++] == piece) {
+//								if(i == arr.length) break;
+//								cnt ++;
+//							}
+//						}
+//					}
+//				}
+//			}
+//		}
+//        if(cnt == arr.length) return true;
+//        else return false;
+		
+		Map<Integer, int[]> map = new HashMap<>();
+		for(int[] piece : pieces) {
+			map.put(piece[0], piece);
 		}
-        if(cnt == arr.length) return true;
-        else return false;
+	
+		for(int i = 0 ; i < arr.length; ) {
+			int curVal = arr[i];
+			if(map.containsKey(curVal)) {
+				int[] piece = map.get(curVal);
+				for(int value : piece) {
+					if(value == arr[i]) i ++;
+					else return false;
+				}
+			} else return false;
+		}
+		
+		return true;
     }
 }
