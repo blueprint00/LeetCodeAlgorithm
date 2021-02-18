8ed1db1cc3284d468ef604d79bc7697025195b0c
blueprint00 <ayun96@naver.com>
2021-02-18 15:55:39 +0900

    Week3 -> day2

--- Week3/KthLargestElementInAnArray.java
+++ Week3/KthLargestElementInAnArray.java
@@ -1,6 +1,11 @@
 package Week3;
 import java.util.*;
 public class KthLargestElementInAnArray {
+	public static void main(String[] args) {
+		int[] nums = {3, 2, 1, 4, 6, 5};
+		int k = 2;
+		System.out.println(findKthLargest(nums, k));
+	}
 	public static int findKthLargest(int[] nums, int k) {
         Arrays.sort(nums);
         return nums[nums.length - k];
