324409c07e3b07b266e16252f6010611de6fb755
blueprint00 <ayun96@naver.com>
2021-02-21 20:03:37 +0900

    Week2 -> day3

--- Week2/CreateSortedArrayThroughInstruction.java
+++ Week2/CreateSortedArrayThroughInstruction.java
@@ -8,29 +8,59 @@ public class CreateSortedArrayThroughInstruction {
 		System.out.println(createSortedArray(instructions));
 	}
 	
-	public static int createSortedArray(int[] instructions) {
-		List<Integer> num = new ArrayList<>();
-		
-		int cost = 0;
-		for(int i = 0; i < instructions.length; i ++) {
-			num.add(instructions[i]);
-
-			Collections.sort(num);
-			for(int n : num) {
-				System.out.print(n + " " );
-			}System.out.println();
-			
-			int max = i;
-			int min = 0;
-			
-			int idx = num.indexOf(instructions[i]);
-			
-			if(num.get(i) == num.get(idx)) idx = i;
-			else if(num.get(0) == num.get(idx)) idx = 0;
-			
-			cost += Math.min(max - idx, idx - min);
+	static int[] count;
+	static int limit = (int)1e5 + 1;
+	static int MOD = (int)1e9 + 7;
+	
+	public static void updateCount(int number) {
+		while(number < limit) {
+			count[number] ++ ;
+			number += number & -number;
 		}
+	}
+	
+	public static int getCount(int number) {
+		int res = 0;
+		while(number > 0) {
+			res += count[number];
+			number -= number & -number;
+		}
+		return res;
+	}
+	
+	public static int createSortedArray(int[] instructions) {
+		int totalCost = 0;
+		count = new int[limit];
 		
-		return cost;
+		for(int i = 0; i < instructions.length; i ++) {
+			int smallerNumCnt = getCount(instructions[i] - 1);
+			int greaterNumCnt = i - getCount(instructions[i]);
+			totalCost = (totalCost + Math.min(smallerNumCnt, greaterNumCnt)) % MOD;
+			updateCount(instructions[i]);
+		}
+		return totalCost;
+		
+//		List<Integer> num = new ArrayList<>();
+//		
+//		int cost = 0;
+//		for(int i = 0; i < instructions.length; i ++) {
+//			num.add(instructions[i]);
+//
+//			Collections.sort(num);
+//			for(int n : num) {
+//				System.out.print(n + " " );
+//			}System.out.println();
+//			
+//			int max = i;
+//			int min = 0;
+//			
+//			int idx = num.indexOf(instructions[i]);
+//			
+//			if(num.get(i) == num.get(idx)) idx = i;
+//			else if(num.get(0) == num.get(idx)) idx = 0;
+//			
+//			cost += Math.min(max - idx, idx - min);
+//		}
+//		return cost;
     }	
 }
